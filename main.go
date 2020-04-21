bd5d758a3efd5599f4d9ec619e344a40726356b5
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-22 01:02:32 +0700

    feat(main): implement grpc server which run go-kit service and endpoint

--- main.go
+++ main.go
@@ -3,6 +3,14 @@ package main
 import (
 	"fmt"
 	"github.com/tech-showcase/entertainment-service/cmd"
+	"github.com/tech-showcase/entertainment-service/global"
+	"github.com/tech-showcase/entertainment-service/model"
+	movieProto "github.com/tech-showcase/entertainment-service/proto/movie"
+	"github.com/tech-showcase/entertainment-service/service"
+	"github.com/tech-showcase/entertainment-service/transport"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"net"
 )
 
 func main() {
@@ -10,5 +18,27 @@ func main() {
 
 	args := cmd.Parse()
 
-	fmt.Println(args)
+	ctx := context.Background()
+
+	config := global.Configuration.Movie
+	movieModel := model.NewMovieModel(config.ServerAddress, config.ApiKey)
+	movieService := service.NewMovieService(movieModel)
+	movieEndpoints := transport.MovieEndpoints{
+		SearchMovieEndpoint: transport.MakeSearchMovieEndpoint(movieService),
+	}
+
+	gRPCServer := grpc.NewServer()
+	movieHandler := transport.NewMovieGRPCServer(ctx, movieEndpoints)
+	movieProto.RegisterMovieServer(gRPCServer, movieHandler)
+
+	portStr := fmt.Sprintf(":%d", args.Port)
+	listener, err := net.Listen("tcp", portStr)
+	if err != nil {
+		panic(err)
+	}
+
+	err = gRPCServer.Serve(listener)
+	if err != nil {
+		panic(err)
+	}
 }
