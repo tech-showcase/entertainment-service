00c9d2abd8fa84c3ace9760470a96465ad96aba7
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-21 16:53:25 +0700

    feat(config): add get config path and parse config func

---
+++ config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"encoding/json"
+	"github.com/tech-showcase/entertainment-service/presenter"
+	"io/ioutil"
+	"os"
+)
+
+func Parse() (config presenter.Config, err error) {
+	configPath := GetPath()
+
+	configFileContent, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(configFileContent, &config)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func GetPath() string {
+	environment := "DEV"
+	if environmentFromEnvVar := os.Getenv("ENVIRONMENT"); environmentFromEnvVar != "" {
+		environment = environmentFromEnvVar
+	}
+
+	configPath := "config/config-dev.json"
+	if configPathFromEnvVar := os.Getenv(environment + "_CONFIG_PATH"); configPathFromEnvVar != "" {
+		configPath = configPathFromEnvVar
+	}
+
+	return configPath
+}
