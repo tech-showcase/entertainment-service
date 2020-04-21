ff74211653097fd03cbdb721e361dffc4a5389ef
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-22 00:42:20 +0700

    feat(model): inject movie model to movie service

--- service/movie.go
+++ service/movie.go
@@ -5,20 +5,23 @@ import (
 )
 
 type (
-	movieService struct{}
+	movieService struct {
+		movieModel model.MovieInterface
+	}
 	MovieService interface {
-		Search(model.MovieInterface, string, int) (model.MovieListPerPage, error)
+		Search(string, int) (model.MovieListPerPage, error)
 	}
 )
 
-func NewMovieService() MovieService {
+func NewMovieService(movieModel model.MovieInterface) MovieService {
 	instance := movieService{}
+	instance.movieModel = movieModel
 
 	return &instance
 }
 
-func (movieService) Search(movieModel model.MovieInterface, keyword string, pageNumber int) (movieData model.MovieListPerPage, err error) {
-	movieData, err = movieModel.Search(keyword, pageNumber)
+func (instance *movieService) Search(keyword string, pageNumber int) (movieData model.MovieListPerPage, err error) {
+	movieData, err = instance.movieModel.Search(keyword, pageNumber)
 	if err != nil {
 		return
 	}
