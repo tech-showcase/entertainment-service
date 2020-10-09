9f0f92b98e61c9077ec2967f7bcdfefd4ef6fa30
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-10-09 15:11:10 +0700

    feat(main): initiate global logger and global tracer

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/tech-showcase/entertainment-service/cmd"
 	"github.com/tech-showcase/entertainment-service/config"
+	"github.com/tech-showcase/entertainment-service/helper"
 )
 
 func init() {
@@ -11,6 +12,13 @@ func init() {
 	config.Instance, err = config.Parse()
 	if err != nil {
 		panic(err)
+	}
+
+	helper.LoggerInstance = helper.NewLogger()
+
+	helper.TracerInstance, _, err = helper.NewTracer(config.Instance.ServiceName)
+	if err != nil {
+		helper.LoggerInstance.Log("NewTracer", err)
 	}
 }
 
