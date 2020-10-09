3e29b9919517c7cbf385e5befb297fa1943a40e3
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-10-09 21:22:02 +0700

    feat(helper): add patched go-kit tracer grpc to ctx

---
+++ helper/patched_go-kit_tracer.go
@@ -0,0 +1,57 @@
+package helper
+
+import (
+	"context"
+	"encoding/base64"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/go-kit/kit/log"
+	stdopentracing "github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+	"google.golang.org/grpc/metadata"
+	"strings"
+)
+
+// GRPCToContext returns a grpc RequestFunc that tries to join with an
+// OpenTracing trace found in `req` and starts a new Span called
+// `operationName` accordingly. If no trace could be found in `req`, the Span
+// will be a trace root. The Span is incorporated in the returned Context and
+// can be retrieved with opentracing.SpanFromContext(ctx).
+func GRPCToContext(tracer stdopentracing.Tracer, operationName string, logger log.Logger) func(ctx context.Context, md metadata.MD) context.Context {
+	return func(ctx context.Context, md metadata.MD) context.Context {
+		var span stdopentracing.Span
+		wireContext, err := tracer.Extract(stdopentracing.TextMap, metadataReaderWriter{&md})
+		if err != nil && err != stdopentracing.ErrSpanContextNotFound {
+			logger.Log("err", err)
+		}
+		spew.Dump(wireContext)
+		span = tracer.StartSpan(operationName, ext.RPCServerOption(wireContext))
+		defer span.Finish()
+		spew.Dump(span)
+		return stdopentracing.ContextWithSpan(ctx, span)
+	}
+}
+
+// A type that conforms to opentracing.TextMapReader and
+// opentracing.TextMapWriter.
+type metadataReaderWriter struct {
+	*metadata.MD
+}
+
+func (w metadataReaderWriter) Set(key, val string) {
+	key = strings.ToLower(key)
+	if strings.HasSuffix(key, "-bin") {
+		val = base64.StdEncoding.EncodeToString([]byte(val))
+	}
+	(*w.MD)[key] = append((*w.MD)[key], val)
+}
+
+func (w metadataReaderWriter) ForeachKey(handler func(key, val string) error) error {
+	for k, vals := range *w.MD {
+		for _, v := range vals {
+			if err := handler(k, v); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
