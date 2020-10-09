0f1446976ebc8d0ebf3c40f9cff70a66c9911270
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-10-09 15:10:42 +0700

    feat(helper): add tracer

---
+++ helper/tracer.go
@@ -0,0 +1,34 @@
+package helper
+
+import (
+	stdopentracing "github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-client-go"
+	"github.com/uber/jaeger-client-go/config"
+	"io"
+	"time"
+)
+
+var TracerInstance stdopentracing.Tracer
+
+func NewTracer(serviceName string) (stdopentracing.Tracer, io.Closer, error) {
+	cfg := config.Configuration{
+		ServiceName: serviceName,
+		Sampler: &config.SamplerConfig{
+			Type:  "const",
+			Param: 1,
+		},
+		Reporter: &config.ReporterConfig{
+			LogSpans:            true,
+			BufferFlushInterval: 1 * time.Second,
+			LocalAgentHostPort:  "localhost:5775",
+		},
+	}
+
+	tracer, closer, err := cfg.NewTracer(
+		config.Logger(jaeger.StdLogger),
+	)
+
+	stdopentracing.SetGlobalTracer(tracer)
+
+	return tracer, closer, err
+}
