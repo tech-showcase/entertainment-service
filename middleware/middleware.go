6d6dc27c0028edcd67d226745ced3b0ff17a0355
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-10-17 12:15:38 +0700

    feat(middleware): add logger middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,12 +1,25 @@
 package middleware
 
 import (
+	"context"
 	"github.com/go-kit/kit/endpoint"
+	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/tracing/opentracing"
 	stdopentracing "github.com/opentracing/opentracing-go"
+	"time"
 )
 
 func ApplyTracerClient(operationName string, endpoint endpoint.Endpoint, tracer stdopentracing.Tracer) (wrappedEndpoint endpoint.Endpoint) {
 	wrappedEndpoint = opentracing.TraceClient(tracer, operationName)(endpoint)
 	return
+}
+
+func ApplyLogger(operationName string, nextEndpoint endpoint.Endpoint, logger log.Logger) (wrappedEndpoint endpoint.Endpoint) {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		defer func(begin time.Time) {
+			logger.Log("operationName", operationName, "elapsedTime", time.Since(begin))
+		}(time.Now())
+
+		return nextEndpoint(ctx, request)
+	}
 }
