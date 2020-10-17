25adf700fc8690f4dff0a7fc46e52cb409034cb6
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-10-17 12:15:11 +0700

    feat(helper): add file logger

--- helper/logger.go
+++ helper/logger.go
@@ -15,3 +15,17 @@ func NewLogger() log.Logger {
 
 	return logger
 }
+
+func NewFileLogger(filepath string) (log.Logger, error) {
+	f, err := os.OpenFile(filepath, os.O_RDWR|os.O_APPEND|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		return nil, err
+	}
+
+	var logger log.Logger
+	logger = log.NewLogfmtLogger(f)
+	logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+	logger = log.With(logger, "caller", log.DefaultCaller)
+
+	return logger, nil
+}
